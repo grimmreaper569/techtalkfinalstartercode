0705984039cafee6802e35bafd09d34a2261a7e2
William Felix <williamfelix569@gmail.com>
2017-07-28 16:02:28 +0000

    jcole

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // JavaScript File
 // JavaScript File
 $( document ).ready(function() {
+ 
+    $("button").click(function(){
+        $("body").append("<p>For your eyez only</p>");
 
+       
+});
 
 });
+
+/*
+var img = document.createElement("img");
+img.src = "http://www.google.com/intl/en_com/images/logo_plain.png";
+
+*/
+
+
+
